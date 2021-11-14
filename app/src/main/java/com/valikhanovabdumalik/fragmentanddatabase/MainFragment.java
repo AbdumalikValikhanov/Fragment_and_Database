ae85e57565d078dc9ef97f3ab20e889174941395
Lenovo-PC <valikhanovabdumalik@gmail.com>
2021-11-14 21:56:08 +0500

    Fragment Ishladi

--- app/src/main/java/com/valikhanovabdumalik/fragmentanddatabase/MainFragment.java
+++ app/src/main/java/com/valikhanovabdumalik/fragmentanddatabase/MainFragment.java
@@ -1,7 +1,9 @@
 package com.valikhanovabdumalik.fragmentanddatabase;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.Toast;
@@ -11,21 +13,43 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.valikhanovabdumalik.fragmentanddatabase.Room.Contact_Fragment;
+import com.valikhanovabdumalik.fragmentanddatabase.databinding.MainFragmentLayoutBinding;
+
 public class MainFragment extends Fragment {
+   MainFragmentLayoutBinding binding;
     public MainFragment() {
-        super(R.layout.main_fragment_layout);
+//        super(R.layout.main_fragment_layout);
+    }
+
+    @Nullable
+
+    @Override
+    public View onCreateView( LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.main_fragment_layout,container,false);
+        binding=MainFragmentLayoutBinding.bind(view);
+//        binding =MainFragmentLayoutBinding.inflate(inflater,container,false);
+//        View view=binding.getRoot();
+        return view;
     }
 
     @Override
     public void onViewCreated(View view,  Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Button button=view.findViewById(R.id.viewPagerbtn);
-        button.setOnClickListener(v -> {
+
+        binding.viewPagerbtn.setOnClickListener(v -> {
             Toast.makeText(getContext(),"Salom",Toast.LENGTH_SHORT).show();
             getActivity().getSupportFragmentManager().beginTransaction()
                     .setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
                     .addToBackStack("")
                     .replace(R.id.main_container,new ViewPagerFragment())
+                    .commit();
+        });
+        binding.roomDatabasebtn.setOnClickListener(v -> {
+            getActivity().getSupportFragmentManager().beginTransaction()
+                    .setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right)
+                    .addToBackStack("")
+                    .replace(R.id.main_container,new Contact_Fragment())
                     .commit();
         });
     }
---
+++ app/src/main/java/com/valikhanovabdumalik/fragmentanddatabase/Room/Contact_Fragment.java
@@ -0,0 +1,22 @@
+package com.valikhanovabdumalik.fragmentanddatabase.Room;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.valikhanovabdumalik.fragmentanddatabase.R;
+import com.valikhanovabdumalik.fragmentanddatabase.databinding.ContactFragmentLayoutBinding;
+
+public class Contact_Fragment extends Fragment {
+    ContactFragmentLayoutBinding binding;
+    public Contact_Fragment() {
+        super(R.layout.contact_fragment_layout);
+    }
+
+
+}
